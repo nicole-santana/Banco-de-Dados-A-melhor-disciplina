bac48895055e6ede79dec60ce1282269d9b73db1
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-25 11:35:16 -0300

    ex3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -33,3 +33,29 @@ CALL sp_LivrosPorCategoria('Ficção Científica');
 CALL sp_LivrosPorCategoria('História');
 CALL sp_LivrosPorCategoria('Autoajuda');
 
+
+-- EX 3:
+
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(200), OUT total_livros INT)
+BEGIN
+    SELECT COUNT(*) INTO total_livros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
+//
+
+DELIMITER ;
+
+CALL sp_ContarLivrosPorCategoria('Romance', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ciência', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Ficção Científica', @total_livros);
+CALL sp_ContarLivrosPorCategoria('História', @total_livros);
+CALL sp_ContarLivrosPorCategoria('Autoajuda', @total_livros);
+
+
+
+-- Assim como no ex2, eu fiz com todas as categorias
+SELECT @total_livros;
