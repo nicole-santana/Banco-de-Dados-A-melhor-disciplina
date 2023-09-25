d68bf27151ffdbe3bd89dcd2c8ee6d6e0a9f6b7a
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-25 11:31:35 -0300

    ex2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -11,3 +11,25 @@ END;
 
 DELIMITER ;
 CALL sp_ListarAutores();
+
+-- Eu tive que fazer uma gambiarra com o git, aí ele criou o readMe por causa disso, e também criou o primeiro commit sozinho
+
+-- EX 2:
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(200))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID WHERE Categoria.Nome = categoria_nome
+END;
+//
+
+DELIMITER ;
+
+
+CALL sp_LivrosPorCategoria('Romance');
+CALL sp_LivrosPorCategoria('Ciência');
+CALL sp_LivrosPorCategoria('Ficção Científica');
+CALL sp_LivrosPorCategoria('História');
+CALL sp_LivrosPorCategoria('Autoajuda');
+
